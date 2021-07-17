895cb1ab25eeb7d2e8ea9196893a1070012f47b2
Condor <1229512018@qq.com>
2021-07-17 13:35:43 +0800

    update

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="datail">
-    <detail-nav-bar class="detail-nav"> </detail-nav-bar>
+    <detail-nav-bar class="detail-nav" @titleClick="titleClick">
+    </detail-nav-bar>
     <div class="wrapper">
-      <scroll class="content">
+      <scroll class="content" ref="scroll">
         <detail-swiper :top-images="topImages"></detail-swiper>
         <detail-base-info :goods="goods"></detail-base-info>
         <detail-shop-info :shop="shop"></detail-shop-info>
-        <detail-goods-info :detail-info="detailInfo"></detail-goods-info>
-        <detail-param-info :param-info="paramInfo"></detail-param-info>
-        <detail-comment-info :comment-info="commentInfo"></detail-comment-info>
-        <goods-list :goods="recommends"></goods-list>
+        <detail-goods-info
+          :detail-info="detailInfo"
+          @imageLoad="imageLoad"
+        ></detail-goods-info>
+        <detail-param-info
+          :param-info="paramInfo"
+          ref="params"
+        ></detail-param-info>
+        <detail-comment-info
+          :comment-info="commentInfo"
+          ref="comment"
+        ></detail-comment-info>
+        <goods-list :goods="recommends" ref="recommend"></goods-list>
       </scroll>
     </div>
   </div>
@@ -27,6 +37,7 @@ import DetailCommentInfo from "./childComps/DetailCommentInfo.vue";
 import Scroll from "components/common/scroll/Scroll";
 import GoodsList from "components/content/goods/GoodsList";
 
+import { debounce } from "common/utils";
 import {
   getDetail,
   getRecommend,
@@ -57,6 +68,8 @@ export default {
       paramInfo: {},
       commentInfo: {},
       recommends: [],
+      themeTopYs: [],
+      getThemeTopY: null,
     };
   },
   created() {
@@ -98,6 +111,39 @@ export default {
       // console.log(res);
       this.recommends = res.data.list;
     });
+
+    //4、给getThemeTopY赋值
+    this.getThemeTopY = debounce(() => {
+      this.themeTopYs.push(0);
+      this.themeTopYs.push(this.$refs.params.$el.offsetTop);
+      this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+      this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+
+      console.log(this.themeTopYs);
+    }, 200);
+  },
+  mounted() {
+    // this.themeTopYs.push(0);
+    // this.themeTopYs.push(this.$refs.params.$el.offsetTop);
+    // this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+    // this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+  },
+  updated() {
+    // this.themeTopYs.push(0);
+    // this.themeTopYs.push(this.$refs.params.$el.offsetTop);
+    // this.themeTopYs.push(this.$refs.comment.$el.offsetTop);
+    // this.themeTopYs.push(this.$refs.recommend.$el.offsetTop);
+  },
+  methods: {
+    imageLoad() {
+      // console.log("123");
+      this.$refs.scroll.refresh();
+      this.getThemeTopY();
+    },
+    titleClick(index) {
+      // console.log(index);
+      this.$refs.scroll.scrollTo(0, -this.themeTopYs[index] + 44, 200);
+    },
   },
 };
 </script>
