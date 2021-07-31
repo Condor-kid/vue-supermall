99f1841a4efbc922893091c26adfe9fd71329c02
Condor <1229512018@qq.com>
2021-07-31 17:15:01 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import LazyLoad from 'vue-lazyload'
 
 import FastClick from 'fastclick'
 import toast from 'components/common/toast'
@@ -13,6 +14,9 @@ Vue.config.productionTip = false
 //安装toast插件
 Vue.use(toast);
 Vue.use(pay);
+Vue.use(LazyLoad, {
+  loading: require('./assets/img/common/placeholder.png')
+});
 
 //解决移动端300ms延迟
 FastClick.attach(document.body);
