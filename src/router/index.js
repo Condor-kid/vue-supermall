7cee71ffd6326581af98f636845aaf3648a98592
Condor <1229512018@qq.com>
2021-07-31 19:42:29 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -37,5 +37,5 @@ const routes = [
 ];
 export default new Router({
     routes,
-    mode: 'history',
+    mode: 'hash',
 })
--- vue.config.js
+++ vue.config.js
@@ -8,6 +8,8 @@ module.exports = {
                 'network': '@/network',
                 'views': '@/views'
             }
-        }
-    }
+        },
+    },
+    publicPath: "./",
+
 }
